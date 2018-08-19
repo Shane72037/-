ca70d5236e795d92027c98c3d20a9ed397d5ef74
謝清荃 <shane72037@icloud.com>
2018-08-19 15:10:52 +0800

    Initial Commit

---
+++ 世界末日/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  世界末日
+//
+//  Created by 謝清荃 on 2018/8/19.
+//  Copyright © 2018年 Shane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
